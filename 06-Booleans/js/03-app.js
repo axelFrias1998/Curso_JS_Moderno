415304c48c6b95f0a5f0ac62a90d65e40d20b46b
Axel Frias Hernandez <axel.frias257@gmail.com>
2021-07-24 22:21:25 -0500

    Booleans en JavaScript terminado

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = false;
+
+if(autenticado) {
+    console.log("Sí puedes ver Netflix");
+}
+else{
+    console.log("No, no puedes verlo");
+}
+
+//Operador ternario
+console.log(autenticado ? "Sí está autenticado" : "No está autenticado");
